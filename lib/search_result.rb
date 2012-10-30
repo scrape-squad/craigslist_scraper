445b1f9cbcd09d95afaaea741806ace394023141
dbc-apprentice <apprentice@devbootcamp.com>
2012-10-29 23:58:58 -0700

    added query results method

--- lib/search_result.rb
+++ lib/search_result.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'uri'
 require_relative './post.rb'
 
 class SearchResult
@@ -9,13 +10,32 @@ class SearchResult
   end
 
   def open_url(url)
-    @search_page = Nokogiri::HTML(open(url))
+    @url = url
+    @search_page = Nokogiri::HTML(open(@url))
   end
 
   def parse
     @search_page.css(".row").each { |post| @posts << Post.from_url(post.at_css("a")[:href]) }
     @posts
   end
+
+  def search_parameter
+    uri = URI.parse(@url)
+    uri.query.split("&").map do |element|
+      if element =~ /query[=](.*)/
+        @query = $1
+        return @query.split("+") #should this be array or string?
+      end
+    end
+  end
+
+  # def to_db
+  #   @db.execute <<-SQL
+  #     INSERT INTO search_results
+  #     VALUES ("#{@}")
+
+  #   SQL
+  # end
 
   private
   def search_to_post_db
--- spec/search_result_spec.rb
+++ spec/search_result_spec.rb
@@ -5,9 +5,9 @@ describe SearchResult do
   let(:searchresult) { SearchResult.new }
 
   before do
-    url = "http://sfbay.craigslist.org/search/?areaID=1&subAreaID=&query=futon&catAbb=sss"
-    FakeWeb.register_uri(:get, url, :body => IO.read("./spec/dummy.html"))
-    searchresult.open_url(url)
+    @url = "http://sfbay.craigslist.org/search/?areaID=1&subAreaID=&query=futon&catAbb=sss"
+    FakeWeb.register_uri(:get, @url, :body => IO.read("./spec/dummy.html"))
+    searchresult.open_url(@url)
     post_mock = mock "Post"
     Post.stub!(:from_url).and_return(post_mock)
   end
@@ -20,9 +20,7 @@ describe SearchResult do
 
   context "#open_url" do
     it "should receive a valid url" do
-      url = "http://sfbay.craigslist.org/search/?areaID=1&subAreaID=&query=futon&catAbb=sss"
-      FakeWeb.register_uri(:get, url, :body => IO.read('./spec/dummy.html'))
-      searchresult.open_url(url).should be_an_instance_of Nokogiri::HTML::Document
+      searchresult.open_url(@url).should be_an_instance_of Nokogiri::HTML::Document
     end
   end
 
@@ -30,6 +28,12 @@ describe SearchResult do
     it "should find all post description urls" do
       searchresult.parse
       searchresult.posts.size.should eq 100
+    end
+  end
+
+  context "#search_parameter" do
+    it "should find all search parameters" do
+      searchresult.search_parameter.should eq "futon"
     end
   end
 end
